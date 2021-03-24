450ea5084072aa5af36f5d6becdccb93787a7b97
aymenkrioudj <47139904+aymenkrioudj@users.noreply.github.com>
2021-03-24 10:48:53 +0100

    intialisation d'environement et splash screen

---
+++ lib/screens/loginPage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:asiproject/constants.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: colorWhite,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
